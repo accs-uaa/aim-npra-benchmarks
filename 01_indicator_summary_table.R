54f3456dc9e4808fb7633ba327baa6a366aede1c
Amanda Droghini <adroghini@alaska.edu>
2024-02-06 20:33:43 -0900

    Update summary & soil horizon query
    Dropped unnecessary columns from the soil horizons query; added 98th percentile as height indicators; completed indicators for soil metrics and soil horizons

--- 01_indicator_summary_table.R
+++ 01_indicator_summary_table.R
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
 # Summarize AIM indicators
-# Author: Timm Nawrocki, Alaska Center for Conservation Science
+# Author: Amanda Droghini, Timm Nawrocki, Alaska Center for Conservation Science
 # Last Updated: 2024-02-06
 # Usage: Script should be executed in R 4.3.2+.
 # Description: "Summarize AIM indicators" creates an indicator summary table from the AKVEG Database.
@@ -37,7 +37,7 @@ shrub_file = path(query_folder, 'npra_11_shrub_query.sql')
 environment_file = path(query_folder, 'npra_12_environment_query.sql')
 soil_metrics_file = path(query_folder, 'npra_13_soil_metrics_query.sql')
 soil_horizons_file = path(query_folder, 'npra_14_soil_horizons_query.sql')
-herbaceous_file = path(data_folder,"15_herbaceousstructure_aimvarious2021.csv")
+herbaceous_file = path(data_folder, '15_herbaceousstructure_aimvarious2021.csv')
 
 # Define output files
 output_file = path(data_folder, 'AIM_NPRA_Indicator_Summary.csv')
@@ -97,18 +97,19 @@ indicators = abiotic_data %>%
 
 # Calculate environment indicators ----
 
-# 1. depth_moss_duff_cm and depth_15_percent_coarse_fragments_cm
+# Summary of depth_15_percent_coarse_fragments_cm not included since most sites (248/261) have a value of -999 for that variable
+
+# 1. depth_moss_duff_cm 
 # No filtering or summarizaton needed, simply append to existing indicators table
-# Do you want to convert -999 values to NA?
+# Replace -999 (null) values with NA
 indicators = environment_data %>% 
   group_by(site_visit_code) %>% 
-  mutate(depth_moss_duff_cm = gsub(pattern=-999,replacement=NA,x=depth_moss_duff_cm),
-         depth_15_percent_coarse_fragments_cm = gsub(pattern=-999,
-                                                     replacement=NA,
-                                                     x=depth_15_percent_coarse_fragments_cm)) %>% 
-  select(site_visit_code, depth_moss_duff_cm, depth_15_percent_coarse_fragments_cm) %>%
-  left_join(indicators, by = "site_visit_code")%>% 
+  mutate(depth_moss_duff_cm = na_if(depth_moss_duff_cm, -999)) %>%
+  select(site_visit_code, depth_moss_duff_cm) %>%
+  right_join(indicators, by = "site_visit_code")%>% 
   arrange(site_visit_code)
+
+summary(indicators$depth_moss_duff_cm)
 
 # 2. depth_active_layer_cm
 # restrictive_type == permafrost & sites with -999 for depth or restrictive_type != permafrost should be dropped
@@ -124,9 +125,10 @@ indicators = environment_data %>%
   right_join(indicators,by="site_visit_code") %>% 
   arrange(site_visit_code)
 
+summary(indicators$depth_active_layer_cm)
+
 # 3. surface_water_depth_cm 
-# (drop -999 and positive (+) values; make negative values positive)
-# Do you want to keep zero values?
+# Ddrop -999 and positive (+) values; make negative values positive
 indicators = environment_data %>% 
   group_by(site_visit_code) %>% 
   filter(depth_water_cm != -999 & depth_water_cm <= 0) %>% 
@@ -140,7 +142,8 @@ summary(indicators$surface_water_depth_cm)
 
 # Calculate ground cover indicators ----
 
-# Calculate surface_water_cover_percent (ground_element == water) and biotic_cover_percent (ground_element == biotic)
+# Calculate surface_water_cover_percent (ground_element == water)
+# Calculate biotic_cover_percent (ground_element == biotic)
 indicators = ground_data %>% 
   group_by(site_visit_code) %>% 
   filter(ground_element == "water" | ground_element == "biotic") %>% 
@@ -152,28 +155,127 @@ indicators = ground_data %>%
   arrange(site_visit_code)
 
 # Calculate herbaceous height indicator ----
-# TWN: There is one site "ADST-72" that has PF04 as name_original, becomes 'herbaceous' as name_adjudicated, is that coded appropriately?
 indicators = herbaceous_data %>% 
-  group_by(site_visit_code) %>% 
-  filter(name_original == "herbaceous" & height_type == "point-intercept mean") %>% # Missing CPBWM-29
-  rename(herbaceous_mean_height_cm = height_cm) %>% 
-  select(site_visit_code,herbaceous_mean_height_cm) %>% 
+  group_by(site_visit_code) %>%
+  filter(name_adjudicated == "herbaceous") %>% # Missing CPBWM-29
+  pivot_wider(id_cols = site_visit_code, 
+              names_from = height_type, 
+              values_from = height_cm) %>% 
+  rename(herbaceous_mean_height_cm = "point-intercept mean",
+         herbaceous_max_height_cm = "point-intercept 98th percentile") %>% 
+  select(site_visit_code,herbaceous_mean_height_cm, herbaceous_max_height_cm) %>% 
   right_join(indicators, by = "site_visit_code") %>% 
   arrange(site_visit_code)
 
 # QA/QC
 summary(indicators$herbaceous_mean_height_cm) # One site missing (CPBWM-29)
+summary(indicators$herbaceous_max_height_cm) # All values greater than mean heights
 
 # Calculate shrub height indicator ----
 indicators = shrub_data %>% 
   group_by(site_visit_code) %>% 
-  filter(name_accepted == "shrub" & height_type == "point-intercept mean") %>% 
-  rename(shrub_mean_height_cm = height_cm) %>% 
-  select(site_visit_code,shrub_mean_height_cm) %>% 
-  right_join(indicators, by = "site_visit_code") %>% 
+  filter(name_accepted == "shrub") %>% 
+  pivot_wider(id_cols = site_visit_code,
+              names_from = height_type, 
+              values_from = height_cm) %>% 
+  rename(shrub_mean_height_cm = "point-intercept mean",
+         shrub_max_height_cm = "point-intercept 98th percentile") %>% 
+  select(site_visit_code,shrub_mean_height_cm, shrub_max_height_cm) %>% 
+  right_join(indicators, by = "site_visit_code") %>%
+  mutate(shrub_mean_height_cm = case_when(site_visit_code != "CPBWM-29_20130731" 
+                                          & is.na(shrub_mean_height_cm) ~ 0,
+                                          .default = shrub_mean_height_cm),
+         shrub_max_height_cm = case_when(site_visit_code != "CPBWM-29_20130731" 
+                                         & is.na(shrub_max_height_cm) ~ 0,
+                                         .default = shrub_max_height_cm)) %>% 
   arrange(site_visit_code)
 
 # QA/QC
-summary(indicators$shrub_mean_height_cm) # Missing data from 17 sites
+summary(indicators$shrub_mean_height_cm) # Should be only one NA from CPBWM-29; all others were converted to zero
+summary(indicators$shrub_max_height_cm) # All values greater than mean heights
 
-# 
+# Calculate shrub/herb ratio height indicator ----
+indicators = indicators %>% 
+  group_by(site_visit_code) %>% 
+  mutate(shrub_herbaceous_height_ratio = case_when(!is.na(shrub_mean_height_cm) & !is.na(herbaceous_mean_height_cm) ~
+                                                     shrub_mean_height_cm / 
+                                                     (shrub_mean_height_cm + herbaceous_mean_height_cm + 0.001),
+                                                   is.na(shrub_mean_height_cm) | is.na(herbaceous_mean_height_cm) ~ NA)) %>% 
+  mutate(shrub_herbaceous_height_ratio = round(shrub_herbaceous_height_ratio, digits = 2))
+
+# QA/QC
+summary(indicators$shrub_herbaceous_height_ratio) # 1 missing site; ratio bounded by 0 and 1
+
+# Calculate soil metrics indicator ----
+
+# Order of priority for value to retain: if water measurement exists, use it; then if 10 cm measurement exists, use it; then if 30 cm measurement exists, use it.
+
+min_soil_depth = soil_metrics_data %>% 
+  filter(ph != -999) %>%
+  group_by(site_visit_code) %>% 
+  summarize(min_depth = min(measure_depth_cm))
+
+indicators = soil_metrics_data %>%
+  filter(ph != -999) %>%
+  left_join(min_soil_depth, by="site_visit_code") %>% 
+  filter(measure_depth_cm == min_depth) %>% 
+  rename(soil_pH = ph) %>% 
+  select(site_visit_code,soil_pH) %>% 
+  right_join(indicators, by = "site_visit_code") %>% 
+  arrange(site_visit_code)
+
+summary(indicators$soil_pH) # 6 sites with NAs
+
+soil_metrics_data %>% filter(ph == -999) %>% nrow() # 8 entries with null values, but 2 sites had 2 entries each (6 unique sites)
+
+# Calculate soil horizons indicators ----
+
+# Calculate organic depth
+indicators = soil_horizons_data %>% 
+  group_by(site_visit_code) %>% 
+  filter(horizon_primary_code=="O") %>% 
+  mutate(thickness_cm = case_when(thickness_cm == -999 ~ depth_lower_cm - depth_upper_cm,
+                                  .default = thickness_cm)) %>% 
+  summarise(organic_depth_cm = sum(thickness_cm)) %>% 
+  right_join(indicators, by = "site_visit_code") %>% 
+  arrange(site_visit_code)
+
+summary(indicators$organic_depth_cm)
+
+# Calculate organic:mineral ratio
+# Restrict to the first 30 cm of soil
+indicators = soil_horizons_data %>%
+  mutate(horizon_type = case_when(horizon_primary_code == 'O' ~ 'organic',
+                                  horizon_primary_code != 'O' ~ 'mineral')) %>%
+  group_by(site_visit_code, horizon_type) %>%
+  filter(depth_upper_cm < 30) %>%
+  mutate(depth_lower_cm = case_when(depth_lower_cm > 30 ~ 30,
+                                    TRUE ~ depth_lower_cm)) %>%
+  filter(depth_lower_cm != -999) %>%
+  mutate(thickness_cm = depth_lower_cm - depth_upper_cm) %>%
+  select(site_visit_code, horizon_primary_code, thickness_cm) %>%
+  summarize(thickness_sum_cm = sum(thickness_cm)) %>%
+  pivot_wider(names_from = horizon_type, values_from = thickness_sum_cm) %>%
+  mutate(organic_mineral_ratio_30_cm = case_when(is.na(organic) ~ 0,
+                                                 is.na(mineral) ~ 1,
+                                                 !is.na(organic) & !is.na (mineral) ~
+                                                   organic/(organic + mineral + 0.001),
+                                                 TRUE ~ -999)) %>%
+  mutate(organic_mineral_ratio_30_cm = round(organic_mineral_ratio_30_cm, 2)) %>%
+  select(-c(organic, mineral)) %>% 
+  right_join(indicators, by = "site_visit_code") %>% 
+  arrange(site_visit_code)
+
+summary(indicators$organic_mineral_ratio_30_cm) # 4 sites with missing values; ratio bounded by 0 and 1
+
+# Re-arrange columns
+# So that the shrub/herbaceous heights columns are all together
+indicators = indicators %>% 
+  select(site_visit_code, organic_mineral_ratio_30_cm, organic_depth_cm,
+         soil_pH,shrub_herbaceous_height_ratio,everything())
+
+# Export to CSV
+write_csv(indicators,output_file)
+
+# Clear workspace
+rm(list=ls())
--- queries/npra_14_soil_horizons_query.sql
+++ queries/npra_14_soil_horizons_query.sql
@@ -16,18 +16,6 @@ SELECT soil_horizons.soil_horizon_id as soil_horizon_id
      , soil_horizons.depth_upper_cm as depth_upper_cm
      , soil_horizons.depth_lower_cm as depth_lower_cm
      , soil_horizons.horizon_primary_code as horizon_primary_code
-     , soil_horizons.horizon_suffix_1_code as horizon_suffix_1
-     , soil_horizons.horizon_suffix_2_code as horizon_suffix_2
-     , soil_horizons.horizon_secondary_code as horizon_secondary_code
-     , soil_horizons.horizon_suffix_3_code as horizon_suffix_3
-     , soil_horizons.horizon_suffix_4_code as horizon_suffix_4
-     , soil_horizons.clay_percent as clay_percent
-     , soil_horizons.total_coarse_fragment_percent as total_coarse_fragment_percent
-     , soil_horizons.gravel_percent as gravel_percent
-     , soil_horizons.cobble_percent as cobble_percent
-     , soil_horizons.stone_percent as stone_percent
-     , soil_horizons.boulder_percent as boulder_percent
 FROM soil_horizons
-    LEFT JOIN soil_texture ON soil_horizons.texture_code = soil_texture.soil_texture_code
     LEFT JOIN site_visit ON soil_horizons.site_visit_code = site_visit.site_visit_code
 WHERE project_code IN ('aim_npra_2017', 'aim_gmt2_2021');
